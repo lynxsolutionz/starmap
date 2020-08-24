4056f03d547c26cdc4e36e4778b0a4162ab91b0f
Mudassar045 <mudassaralics007@gmail.com>
2020-08-24 23:52:59 +0500

    Updates

---
+++ precache-manifest.38b9d23385e96119ffb335411be30cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9874b2d1a27144cbdff5cb18ea8f454",
+    "url": "/starmap/index.html"
+  },
+  {
+    "revision": "ff3a8df32dd0a3a7dc7c",
+    "url": "/starmap/static/css/main.a414de92.chunk.css"
+  },
+  {
+    "revision": "3cec4726c4c47c48aa97",
+    "url": "/starmap/static/js/2.5fb841ad.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/starmap/static/js/2.5fb841ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3a8df32dd0a3a7dc7c",
+    "url": "/starmap/static/js/main.6976c7ec.chunk.js"
+  },
+  {
+    "revision": "035fc2681acdaf25b228",
+    "url": "/starmap/static/js/runtime-main.f34f6148.js"
+  },
+  {
+    "revision": "f07e5eaba43268cb5a4d40eb18d84b7c",
+    "url": "/starmap/static/media/machine-learning.f07e5eab.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7bf7c710757adb374ce9615e89f1816.js"
+  "/starmap/precache-manifest.38b9d23385e96119ffb335411be30cc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starmap/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
